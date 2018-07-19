9b81a47fec10f7896660222158f43b42bffb82e8
Josh <joshaaronlevy@gmail.com>
2018-07-19 00:02:40 -0600

    Cleaning up code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,252 +1,258 @@
 var _ = require('lodash');
 const BUILD_PATH = '_output/';
-const ASSETS_PATH = ["images/**/*.{jpg,jpeg,png,gif}", "fonts/**"];
+const ASSETS_PATH = ['images/**/*.{jpg,jpeg,png,gif}', 'fonts/**'];
 const CONFIG = require('./config');
 
 if (!global._babelPolyfill) {
     // require('babel-core/polyfill')
-    require("babel/register")({
-        ignore: /bower_components|node_modules|_output\//,
-        sourceMaps: true
-            //
-    });
+  require('babel/register')({
+    ignore:     /bower_components|node_modules|_output\//,
+    sourceMaps: true
+        //
+  });
 }
 
 var jsLibs = [
-    './bower_components/angular-sanitize/angular-sanitize.min.js',
-    './bower_components/velocity/velocity.js',
-    './bower_components/bootstrap/dist/js/bootstrap.js',
-    './bower_components/html5shiv/dist/html5shiv.js',
-    './bower_components/ckeditor/ckeditor.js',
-    './bower_components/moment/moment.js',
-    './bower_components/ui-select/dist/select.js',
-    './bower_components/angular-spinners/dist/angular-spinners.min.js',
-    './bower_components/ng-ckeditor/ng-ckeditor.js',
-    './js/vendor/repute-scripts.js',
-    './js/vendor/modernizr.js',
-    './js/vendor/animated-signup.js',
-    './js/vendor/jquery-ui.js',
-    './js/vendor/main.js',
-    './js/vendor/hover-dropdown.js',
-    './js/vendor/jquery-easing.js',
-    './js/vendor/scrollReveal.js',
-    './js/vendor/jquery-snippet.js',
-    './js/vendor/jquery-fitvids.js',
-    './js/vendor/activate-snippet.js',
-    './js/vendor/skrollr.min.js',
-    './js/vendor/jquery.ui.widget.js',
-    './js/vendor/jquery.iframe-transport.js',
-    './js/vendor/jquery.fileupload.js'
+  './bower_components/angular-sanitize/angular-sanitize.min.js',
+  './bower_components/velocity/velocity.js',
+  './bower_components/bootstrap/dist/js/bootstrap.js',
+  './bower_components/html5shiv/dist/html5shiv.js',
+  './bower_components/ckeditor/ckeditor.js',
+  './bower_components/moment/moment.js',
+  './bower_components/ui-select/dist/select.js',
+  './bower_components/angular-spinners/dist/angular-spinners.min.js',
+  './bower_components/ng-ckeditor/ng-ckeditor.js',
+  './js/vendor/repute-scripts.js',
+  './js/vendor/modernizr.js',
+  './js/vendor/animated-signup.js',
+  './js/vendor/jquery-ui.js',
+  './js/vendor/main.js',
+  './js/vendor/hover-dropdown.js',
+  './js/vendor/jquery-easing.js',
+  './js/vendor/scrollReveal.js',
+  './js/vendor/jquery-snippet.js',
+  './js/vendor/jquery-fitvids.js',
+  './js/vendor/activate-snippet.js',
+  './js/vendor/skrollr.min.js',
+  './js/vendor/jquery.ui.widget.js',
+  './js/vendor/jquery.iframe-transport.js',
+  './js/vendor/jquery.fileupload.js'
 ];
 
 var wysiwyg = ['./ckeditor/*'];
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     // Force use of Unix newlines
-    grunt.util.linefeed = '\n';
+  grunt.util.linefeed = '\n';
 
     // Project configuration.
-    grunt.initConfig({
+  grunt.initConfig({
 
         // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-        distPath: BUILD_PATH,
+    distPath: BUILD_PATH,
 
-        clean: {
-            dist: BUILD_PATH + ""
+    clean: {
+      dist: BUILD_PATH + ''
+    },
+
+    jade: {
+      options: {
+        pretty: true,
+        data:   _.extend({
+          cacheBreaker: (Date.now() + '').substr(7),
+          assets:       '../' + BUILD_PATH
         },
+        CONFIG)
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd:    'site/',
+          src:    './**/*.jade',
+          dest:   BUILD_PATH,
+          ext:    '.html'
+        }] //{'views/**/*.tmpl.jade': 'public/views/**/*.html'}
+      }
+    },
 
-        jade: {
-            options: {
-                pretty: true,
-                data: _.extend({
-                        cacheBreaker: (Date.now() + '').substr(7),
-                        assets: '../' + BUILD_PATH
-                    },
-                    CONFIG)
-            },
-            build: {
-                files: [{
-                        expand: true,
-                        cwd: 'site/',
-                        src: './**/*.jade',
-                        dest: BUILD_PATH,
-                        ext: '.html'
-                    }] //{'views/**/*.tmpl.jade': 'public/views/**/*.html'}
-            }
-        },
-
-        uglify: {
-            options: {
-                debug: true,
-                prettify: true,
-                data: {
+    uglify: {
+      options: {
+        debug:    true,
+        prettify: true,
+        data:     {
                     // assets: '../' + BUILD_PATH
-                }
-            },
-            vendor: {
-                dest: BUILD_PATH + 'js/vendor.min.js',
-                src: jsLibs
-            },
-            build: {
-                files: [{
-                        expand: true,
-                        cwd: 'js/',
-                        src: ['**/*.js'],
-                        dest: BUILD_PATH + 'js',
-                        ext: '.min.js'
-                    }] //{'views/**/*.tmpl.jade': 'public/views/**/*.html'}
-            }
-
-        },
-
-        copy: {
-            test: {
-                src: './test/**',
-                dest: BUILD_PATH + '',
-                expand: true
-            },
-            favicon: {
-                src: './favicon.ico',
-                dest: BUILD_PATH + '',
-            },
-            data: {
-                src: './data/**',
-                dest: BUILD_PATH + '',
-                expand: true
-            },
-            css: {
-                expand: true,
-                flatten: true,
-                src: [
-                    'css/*.fileupload*.css',
-                    'bower_components/ng-ckeditor/ng-ckeditor.css',
-                    'bower_components/font-awesome/css/font-awesome.min.css',
-                    'bower_components/ideal-image-slider/ideal-image-slider.css',
-                    'bower_components/ideal-image-slider/themes/default/default.css'
-                ],
-                dest: BUILD_PATH + 'css/'
-            },
-            vendor: {
-                files: [{
-                    expand: true,
-                    flatten: false,
-                    src: '**',
-                    cwd: 'bower_components/ckeditor',
-                    dest: BUILD_PATH + 'ckeditor'
-                }, {
-                    expand: true,
-                    flatten: true,
-                    src: [
-                        'bower_components/animate-css/animate.min.css',
-                        'bower_components/bootstrap/dist/css/bootstrap.min.css'
-                    ],
-                    dest: BUILD_PATH + 'css/'
-                }]
-            },
-            assets: {
-                expand: true,
-                src: ASSETS_PATH,
-                dest: BUILD_PATH
-            }
-        },
-
-        less: {
-            options: { "debug": true },
-            main: {
-                files: [{
-                    cwd: './less',
-                    expand: true,
-                    dest: BUILD_PATH + 'styles',
-                    src: './*.less',
-                    ext: '.min.css'
-                }]
-            }
-        },
-
-        browserify: {
-            options: {
-                transform: [
-                    'jadeify', ['babelify', { ignore: /bower_components|node_modules|_output\// }],
-                    'debowerify'
-                ],
-                browserifyOptions: { debug: true },
-                noparse: ['lodash'],
-                ignore: ['angular', 'jquery']
-            },
-            dist: {
-                files: [{
-                    cwd: './js',
-                    expand: true,
-                    dest: BUILD_PATH + 'js',
-                    src: 'apps/**/index.js*',
-                    ext: '.min.js'
-                }, {
-                    cwd: './test',
-                    expand: true,
-                    dest: BUILD_PATH + 'test',
-                    src: './**/*.js',
-                    ext: '.min.js'
-                }]
-            }
-        },
-        concat: {
-            vendor: {
-                dest: BUILD_PATH + 'js/vendor.js',
-                src: jsLibs
-            }
-        },
-        concurrent: {
-            options: {
-                logConcurrentOutput: true
-            },
-            dev: ['watch', 'browserSync']
-        },
-
-        watch: {
-            options: {
-                livereload: true
-            },
-            less: {
-                files: './**/*.{less,css}',
-                tasks: ['less']
-            },
-            test: {
-                files: ['./test/**/*'],
-                tasks: ['copy:test']
-            },
-            js: {
-                files: ['./js/**/*.js', './config/**/*.js'],
-                tasks: ['newer:copy', 'browserify' /*, 'uglify:vendor'*/ ]
-            },
-            jade: {
-                files: ['./site/**/*.jade', './themes/**/*.jade'],
-                tasks: ['jade']
-            }
-        },
-        browserSync: {
-            default_options: {
-                bsFiles: {
-                    src: [BUILD_PATH + "**/*.{css,html,js}"]
-                },
-                options: {
-                    watchTask: false, // set to true if apart of steps ending in 'watch'
-                    // browser: "/opt/google/chrome/chrome",
-                    reloadDelay: 750,
-                    reloadDebounce: 2500,
-                    injectChanges: false,
-                    // port: 4001,
-                    server: BUILD_PATH
-                }
-            }
         }
-    });
+      },
+      vendor: {
+        dest: BUILD_PATH + 'js/vendor.min.js',
+        src:  jsLibs
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd:    'js/',
+          src:    ['**/*.js'],
+          dest:   BUILD_PATH + 'js',
+          ext:    '.min.js'
+        }] //{'views/**/*.tmpl.jade': 'public/views/**/*.html'}
+      }
 
-    require('load-grunt-tasks')(grunt);
+    },
 
-    grunt.registerTask('html', ['jade']);
-    grunt.registerTask('build', ['newer:concat', 'newer:copy', 'browserify' /*, 'uglify:vendor'*/ , 'jade', 'less']);
-    grunt.registerTask('watch', ['browserSync', /*'clean', 'build',*/ 'watch']);
-    grunt.registerTask('default', ['build' /*, 'browserSync'*/ ]);
+    copy: {
+      test: {
+        src:    './test/**',
+        dest:   BUILD_PATH + '',
+        expand: true
+      },
+      favicon: {
+        src:  './favicon.ico',
+        dest: BUILD_PATH + '',
+      },
+      data: {
+        src:    './data/**',
+        dest:   BUILD_PATH + '',
+        expand: true
+      },
+      css: {
+        expand:  true,
+        flatten: true,
+        src:     [
+          'css/*.fileupload*.css',
+          'bower_components/ng-ckeditor/ng-ckeditor.css',
+          'bower_components/font-awesome/css/font-awesome.min.css',
+          'bower_components/ideal-image-slider/ideal-image-slider.css',
+          'bower_components/ideal-image-slider/themes/default/default.css'
+        ],
+        dest: BUILD_PATH + 'css/'
+      },
+      vendor: {
+        files: [{
+          expand:  true,
+          flatten: false,
+          src:     '**',
+          cwd:     'bower_components/ckeditor',
+          dest:    BUILD_PATH + 'ckeditor'
+        }, {
+          expand:  true,
+          flatten: true,
+          src:     [
+            'bower_components/animate-css/animate.min.css',
+            'bower_components/bootstrap/dist/css/bootstrap.min.css'
+          ],
+          dest: BUILD_PATH + 'css/'
+        }]
+      },
+      assets: {
+        expand: true,
+        src:    ASSETS_PATH,
+        dest:   BUILD_PATH
+      }
+    },
+
+    less: {
+      options: {
+        'debug': true
+      },
+      main: {
+        files: [{
+          cwd:    './less',
+          expand: true,
+          dest:   BUILD_PATH + 'styles',
+          src:    './*.less',
+          ext:    '.min.css'
+        }]
+      }
+    },
+
+    browserify: {
+      options: {
+        transform: [
+          'jadeify', ['babelify', {
+            ignore: /bower_components|node_modules|_output\//
+          }],
+          'debowerify'
+        ],
+        browserifyOptions: {
+          debug: true
+        },
+        noparse: ['lodash'],
+        ignore:  ['angular', 'jquery']
+      },
+      dist: {
+        files: [{
+          cwd:    './js',
+          expand: true,
+          dest:   BUILD_PATH + 'js',
+          src:    'apps/**/index.js*',
+          ext:    '.min.js'
+        }, {
+          cwd:    './test',
+          expand: true,
+          dest:   BUILD_PATH + 'test',
+          src:    './**/*.js',
+          ext:    '.min.js'
+        }]
+      }
+    },
+    concat: {
+      vendor: {
+        dest: BUILD_PATH + 'js/vendor.js',
+        src:  jsLibs
+      }
+    },
+    concurrent: {
+      options: {
+        logConcurrentOutput: true
+      },
+      dev: ['watch', 'browserSync']
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      less: {
+        files: './**/*.{less,css}',
+        tasks: ['less']
+      },
+      test: {
+        files: ['./test/**/*'],
+        tasks: ['copy:test']
+      },
+      js: {
+        files: ['./js/**/*.js', './config/**/*.js'],
+        tasks: ['newer:copy', 'browserify' /*, 'uglify:vendor'*/ ]
+      },
+      jade: {
+        files: ['./site/**/*.jade', './themes/**/*.jade'],
+        tasks: ['jade']
+      }
+    },
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [BUILD_PATH + '**/*.{css,html,js}']
+        },
+        options: {
+          watchTask:      false, // set to true if apart of steps ending in 'watch'
+                    // browser: "/opt/google/chrome/chrome",
+          reloadDelay:    750,
+          reloadDebounce: 2500,
+          injectChanges:  false,
+                    // port: 4001,
+          server:         BUILD_PATH
+        }
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('html', ['jade']);
+  grunt.registerTask('build', ['newer:concat', 'newer:copy', 'browserify' /*, 'uglify:vendor'*/ , 'jade', 'less']);
+  grunt.registerTask('watch', ['browserSync', /*'clean', 'build',*/ 'watch']);
+  grunt.registerTask('default', ['build' /*, 'browserSync'*/ ]);
 };
--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,30 +1,23 @@
-var MainController = module.exports = ["$scope", "$timeout", "agentService", "consumerService", "Cache", function($scope, $timeout, agentService, consumerService, Cache) {
+var MainController = module.exports = ['$scope', '$timeout', 'agentService', 'consumerService', 'Cache', function ($scope, $timeout, agentService, consumerService, Cache) {
   $scope.toolbarUI = [
-    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'justifyLeft','justifyCenter','justifyRight','indent','outdent'],
+    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent'],
     ['bold', 'italics', 'underline', 'ul', 'ol', 'undo', 'redo', 'html', 'insertLink', 'clear']
   ];
   var currentUser = Cache.cache.bind(null, 'user');
   var currentAgent = Cache.cache.bind(null, 'agent');
   var currentConsumer = Cache.cache.bind(null, 'consumer');
 
-  // $('.nav-tabs a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-  //       window.location.hash = e.target.hash.substr(1) ;
-  //       return false;
-  // });
-
-  // $scope.showTour = function() {
-  //   introJs().start();
-  // };
-
   var animationDelay = 2000;
 
   animateHeadline($('.cd-headline'));
 
   function animateHeadline($headlines) {
-    $headlines.each(function(){
+    $headlines.each(function () {
       var headline = $(this);
       //trigger animation
-      setTimeout(function(){ hideWord( headline.find('.is-visible') ) }, animationDelay);
+      setTimeout(function () {
+        hideWord(headline.find('.is-visible'))
+      }, animationDelay);
       //other checks here ...
     });
   }
@@ -32,7 +25,9 @@ var MainController = module.exports = ["$scope", "$timeout", "agentService", "co
   function hideWord($word) {
     var nextWord = takeNext($word);
     switchWord($word, nextWord);
-    setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+    setTimeout(function () {
+      hideWord(nextWord)
+    }, animationDelay);
   }
 
   // function takeNext($word) {
@@ -51,61 +46,61 @@ var MainController = module.exports = ["$scope", "$timeout", "agentService", "co
   singleLetters($('.cd-headline.letters').find('b'));
 
   function singleLetters($words) {
-    $words.each(function(){
+    $words.each(function () {
       var word = $(this),
         letters = word.text().split(''),
         selected = word.hasClass('is-visible');
       for (i in letters) {
-        letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
+        letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>' : '<i>' + letters[i] + '</i>';
       }
-        var newLetters = letters.join('');
-        word.html(newLetters);
+      var newLetters = letters.join('');
+      word.html(newLetters);
     });
   }
 
-  $timeout(function(){
+  $timeout(function () {
     var navAlt = $('.nav-alt').children();
     // if (self.location.href.indexOf('/agent/dashboard.html') > -1){
     //   $(navAlt[0]).addClass('active');
     // }
-    if (self.location.href.indexOf('/agent/leads.html') > -1){
+    if (self.location.href.indexOf('/agent/leads.html') > -1) {
       $(navAlt[0]).addClass('active');
     }
-    if (self.location.href.indexOf('/agent/favorites.html') > -1){
+    if (self.location.href.indexOf('/agent/favorites.html') > -1) {
       $(navAlt[1]).addClass('active');
     }
-    if (self.location.href.indexOf('/agent/messages.html') > -1){
+    if (self.location.href.indexOf('/agent/messages.html') > -1) {
       $(navAlt[2]).addClass('active');
     }
-    if (self.location.href.indexOf('/agent/profile.html') > -1){
+    if (self.location.href.indexOf('/agent/profile.html') > -1) {
       $(navAlt[3]).addClass('active');
     }
-  },250);
+  }, 250);
 
-  $timeout(function(){
+  $timeout(function () {
     var navAlt = $('.nav-alt').children();
-    if (self.location.href.indexOf('/consumer/agents.html') > -1){
+    if (self.location.href.indexOf('/consumer/agents.html') > -1) {
       $(navAlt[0]).addClass('active');
     }
-    if (self.location.href.indexOf('/consumer/favorites.html') > -1){
+    if (self.location.href.indexOf('/consumer/favorites.html') > -1) {
       $(navAlt[1]).addClass('active');
     }
-    if (self.location.href.indexOf('/consumer/messages.html') > -1){
+    if (self.location.href.indexOf('/consumer/messages.html') > -1) {
       $(navAlt[2]).addClass('active');
     }
-    if (self.location.href.indexOf('/consumer/profile.html') > -1){
+    if (self.location.href.indexOf('/consumer/profile.html') > -1) {
       $(navAlt[3]).addClass('active');
     }
-  },250);
+  }, 250);
 
   (function init() {
     console.log('Current hash: ', '.nav-tabs a[href="' + location.hash + '"]\n',
       'locationHash: ', location.hash);
 
     if (location.hash !== '') {
-        $('.nav-tabs a[href="' + location.hash + '"]').tab('show');
+      $('.nav-tabs a[href="' + location.hash + '"]').tab('show');
     } else {
-        $('.nav-tabs a:first').tab('show');
+      $('.nav-tabs a:first').tab('show');
     }
     try {
       currentUser();
@@ -113,7 +108,7 @@ var MainController = module.exports = ["$scope", "$timeout", "agentService", "co
       currentConsumer();
       console.log('Initialized $scope.$root Data:', $scope.$root);
     } catch (err) {
-      console.error("ERR", err);
+      console.error('ERR', err);
     }
   })();
 
